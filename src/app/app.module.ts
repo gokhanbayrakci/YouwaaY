98d4e7e35435a20b184934b67bdef0ea6cb8d168
Gökhan BAYRAKCI <gokhanbayrak654@gmail.com>
2018-10-01 15:54:26 +0300

    search page is done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchPageComponent } from './search-page/search-page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchPageComponent,
+    
   ],
   imports: [
     BrowserModule
